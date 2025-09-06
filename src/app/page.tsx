5f32237d4b5fad0b06b40a806b82dd4155fd0b9c
r77tchan <r77tchan@gmail.com>
2025-09-06 18:12:08 +0900

    グローバルスタイルに緑のカラーパレットを追加し、学習セクションを動的にレンダリングするように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
 import Link from "next/link";
 
+const learningSections = [
+  {
+    title: "Learn by Reading",
+    bgColor: "bg-red1",
+    button: {
+      text: "Go to Book List",
+      href: "/book",
+      bgColor: "bg-red2",
+      disabled: false,
+    },
+  },
+  {
+    title: "Learn by Typing",
+    bgColor: "bg-blue1",
+    button: {
+      text: "Coming Soon",
+      href: "",
+      bgColor: "bg-blue2",
+      disabled: true,
+    },
+  },
+  {
+    title: "Learn by News",
+    bgColor: "bg-green1",
+    button: {
+      text: "Go to News List",
+      href: "/news",
+      bgColor: "bg-green2",
+      disabled: false,
+    },
+  },
+];
+
 export default function Home() {
   return (
     <div className="container mx-auto min-h-screen">
@@ -9,21 +42,28 @@ export default function Home() {
         in the future.
       </p>
       <div className="grid grid-cols-1 gap-8 pb-4 lg:grid-cols-2">
-        <div className="bg-red1 rounded text-center outline">
-          <h2 className="px-4 py-8 text-2xl font-bold">Learn by Reading</h2>
-          <Link
-            href="/book"
-            className="bg-red2 hover:bg-hover mb-12 inline-block rounded border px-8 py-4 font-bold transition-all duration-75 hover:shadow-lg"
+        {learningSections.map((section) => (
+          <div
+            key={section.title}
+            className={`${section.bgColor} rounded text-center outline`}
           >
-            Go to Book List
-          </Link>
-        </div>
-        <div className="bg-blue1 rounded text-center outline">
-          <h2 className="px-4 py-8 text-2xl font-bold">Learn by Typing</h2>
-          <span className="bg-blue2 mb-12 inline-block cursor-not-allowed rounded border px-8 py-4 font-bold">
-            Coming Soon
-          </span>
-        </div>
+            <h2 className="px-4 py-8 text-2xl font-bold">{section.title}</h2>
+            {section.button.disabled ? (
+              <span
+                className={`${section.button.bgColor} mb-12 inline-block cursor-not-allowed rounded border px-8 py-4 font-bold`}
+              >
+                {section.button.text}
+              </span>
+            ) : (
+              <Link
+                href={section.button.href}
+                className={`${section.button.bgColor} hover:bg-hover mb-12 inline-block rounded border px-8 py-4 font-bold transition-all duration-75 hover:shadow-lg`}
+              >
+                {section.button.text}
+              </Link>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
