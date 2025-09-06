85d56fcfdfc6269e7cc308fdc37a3d20a61dea8f
r77tchan <r77tchan@gmail.com>
2025-09-06 19:21:22 +0900

    ニュースページコンポーネントを新規作成し、書籍リストを動的に表示する機能を追加

---
+++ src/app/news/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { books } from "@/lib/data/index";
+
+export default function NewsPage() {
+  return (
+    <div className="container mx-auto min-h-screen">
+      <h1 className="px-4 py-4 text-2xl font-bold">News List</h1>
+      <div className="grid grid-cols-1 gap-4 px-4 pb-4 sm:grid-cols-2 lg:grid-cols-3">
+        {books.map((book) => (
+          <Link
+            key={book.id}
+            href={`/book/${book.id}`}
+            className="hover:bg-hover block border p-4 transition-all duration-200 hover:-translate-y-1 hover:shadow-lg"
+          >
+            {book.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
